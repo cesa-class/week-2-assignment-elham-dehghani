a5f4be40f8af6eec55f5735862caa4052c1841b6
elham-dehghani <47833497+elham-dehghani@users.noreply.github.com>
2019-03-01 05:51:41 -0800

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,3 +1,8 @@
 # Main
 if __name__ == "__main__":
     weight = input("Kilos? ")
+    weight = int(weight)
+    if weight%2==0 and weight != 2 :
+        print("yes")
+    else:
+        print("no")
