434fb2edc215391669e1f22f4e8839084d1b2e2b
elham-dehghani <47833497+elham-dehghani@users.noreply.github.com>
2019-03-02 10:29:33 -0800

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,3 +1,14 @@
 # Main
 if __name__ == "__main__":
-    pass
+    n = input("length of square: ")
+    m = input("width of square: ")
+    a = input("length of flagton: ")
+    n = int(n)
+    m = int(m)
+    a = int(a)
+    b = m%a
+    m2= m+a-b
+    c = n%a
+    n2= n+a-c
+    print(m2*n2/a**2)
+    
